0dd2cad8881cd9f0c4c265a48dcbe223d1d949d0
NotePCHikin <rgon1192@gmail.com>
2024-02-25 22:20:29 +0900

    router-setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import CreatPost from "./components/CreatPost";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-blue-400">Hello world!</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/creatpost" element={<CreatPost />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
+      </Routes>
+      <h1 className="text-3xl font-bold underline text-blue-400">
+        Hello world!
+      </h1>
+    </Router>
   );
 }
 
